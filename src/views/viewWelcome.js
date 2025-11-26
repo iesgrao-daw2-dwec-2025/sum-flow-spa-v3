fd2d33bc999b90cc150f762830c82e04fbcf7e94
maarcalcover <146415291+maarcalcover@users.noreply.github.com>
2025-11-26 16:05:37 +0000

    feat: add view by Marc Alcover (UD2-A7)
    * feat: add new view Marc
    
    * Feat: Remove comment in view Marc
    
    * Change button action to navigate to welcome view
    
    * Refactor btnBack event listener for viewWelcome

--- src/views/viewWelcome.js
+++ src/views/viewWelcome.js
@@ -4,13 +4,19 @@ import { $, render, template, html } from "../utils/globals.js";
 // --- Imported views ---
 import { viewLogin } from "./viewLogin.js";
 
+import { accionMarc } from "./viewMarc.js";
+
 // --- View definition ---
 export function viewWelcome() {
   template(html`
     <h1>Aplicación Sum Flow (SPA Modular con globals.js)</h1>
     <p>Bienvenido/a. Pulsa para continuar al login.</p>
     <button id="btnStart">Continuar</button>
+    <button id="btnMarc">Botón Marc</button>
   `);
 
   $("#btnStart")?.addEventListener("click", () => render(viewLogin));
+
+  $("#btnMarc")?.addEventListener("click", () => { accionMarc(); 
+  });
 }
